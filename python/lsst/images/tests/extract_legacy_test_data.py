6c4f20aae8ee7fd7ed1ca48bc048927aac3e9292
Jim Bosch <jbosch@astro.princeton.edu>
2026-02-21 15:33:03 -0800

    Add legacy cell coadd to test data extraction.

--- python/lsst/images/tests/extract_legacy_test_data.py
+++ python/lsst/images/tests/extract_legacy_test_data.py
@@ -27,19 +27,20 @@ try:
         QuantizationOptions,
         ScalingAlgorithm,
     )
+    from lsst.cell_coadds import MultipleCellCoadd
     from lsst.daf.butler import Butler, DatasetRef
     from lsst.geom import Box2I, Extent2I, Point2I
     from lsst.utils import getPackageDir
 except ImportError as err:
     err.add_note(
         "Updating the test data requires a full Rubin development enviroment with at least "
-        "'click', 'afw', 'obs_base', 'meas_extensions_psfex', and 'meas_extensions_piff' importable. "
-        "This is not necessary for just running the tests."
+        "'click', 'afw', 'obs_base', 'meas_extensions_psfex', 'meas_extensions_piff' and 'cell_coadds' "
+        "importable. This is not necessary for just running the tests."
     )
     raise
 
 
-from .data_ids import DP2_VISIT_DETECTOR_DATA_ID
+from .data_ids import DP2_COADD_DATA_ID, DP2_VISIT_DETECTOR_DATA_ID
 
 
 def extract_visit_image(
@@ -90,6 +91,41 @@ def extract_visit_image(
     )
 
 
+def extract_cell_coadd(
+    butler: Butler,
+    output_path: str,
+    dataset_ref: DatasetRef,
+    shuffle: bool,
+) -> None:
+    """Load a subimage of a cell coadd from a butler repository and save it
+    to testdata_images.
+    """
+    full_cell_coadd = butler.get(dataset_ref)
+    cell_coadd = MultipleCellCoadd(
+        [full_cell_coadd.cells[i, j] for j in range(7, 11) for i in range(5, 8)],
+        grid=full_cell_coadd.grid,
+        outer_cell_size=full_cell_coadd.outer_cell_size,
+        psf_image_size=full_cell_coadd.psf_image_size,
+        common=full_cell_coadd.common,
+    )
+    if shuffle:
+        rng = np.random.default_rng()
+        for cell in cell_coadd.cells.values():
+            indices = np.arange(cell.outer.image.array.size, dtype=int)
+            rng.shuffle(indices)
+            cell.outer.image.array[:, :] = cell.outer.image.array.flat[indices].reshape(150, 150)
+            cell.outer.mask.array[:, :] = cell.outer.mask.array.flat[indices].reshape(150, 150)
+            cell.outer.variance.array[:, :] = cell.outer.variance.array.flat[indices].reshape(150, 150)
+            for n in cell.outer.noise_realizations:
+                n.array[:, :] = n.array.flat[indices].reshape(150, 150)
+            if cell.outer.mask_fractions is not None:
+                cell.outer.mask_fractions.array[:, :] = cell.outer.mask_fractions.array.flat[indices].reshape(
+                    150, 150
+                )
+    os.makedirs(os.path.dirname(output_path), exist_ok=True)
+    cell_coadd.writeFits(output_path)
+
+
 def extract_camera(butler: Butler, output_path: str, dataset_ref: DatasetRef) -> None:
     camera = butler.get(dataset_ref)
     os.makedirs(os.path.dirname(output_path), exist_ok=True)
@@ -124,8 +160,30 @@ def extract_test_data() -> None:
     default="LSSTCam/runs/DRP/v30_0_0/DM-53877",
     help="Collection to search for visit_summary datasets used to update the WCS.",
 )
+@click.option(
+    "--visit-images/--no-visit-images",
+    default=True,
+    help="Whether to extract [preliminary_]visit_image datasets.",
+)
+@click.option(
+    "--coadds/--no-coadds",
+    default=True,
+    help="Whether to extract coadd datasets.",
+)
+@click.option(
+    "--camera/--no-camera",
+    default=True,
+    help="Whether to extract the camera.",
+)
 def extract_dp2(
-    butler_repo: str | None, testdata_dir: str | None, collection: str, wcs_collection: str
+    butler_repo: str | None,
+    testdata_dir: str | None,
+    collection: str,
+    wcs_collection: str,
+    *,
+    visit_images: bool,
+    coadds: bool,
+    camera: bool,
 ) -> None:
     """Extract test data from a butler repository."""
     if butler_repo is None:
@@ -133,41 +191,55 @@ def extract_dp2(
     if testdata_dir is None:
         testdata_dir = getPackageDir("testdata_images")
     butler = Butler.from_config(butler_repo, collections=[collection])
-    extract_visit_image(
-        butler,
-        os.path.join(
-            testdata_dir,
-            "dp2",
-            "legacy",
-            "visit_image.fits",
-        ),
-        find_dataset_or_raise(butler, "visit_image", **DP2_VISIT_DETECTOR_DATA_ID),
-        shuffle=True,
-        wcs_dataset_ref=find_dataset_or_raise(
-            butler, "visit_summary", **DP2_VISIT_DETECTOR_DATA_ID, collections=wcs_collection
-        ),
-    )
-    extract_visit_image(
-        butler,
-        os.path.join(
-            testdata_dir,
-            "dp2",
-            "legacy",
-            "preliminary_visit_image.fits",
-        ),
-        find_dataset_or_raise(butler, "preliminary_visit_image", **DP2_VISIT_DETECTOR_DATA_ID),
-        shuffle=True,
-    )
-    extract_camera(
-        butler,
-        os.path.join(
-            testdata_dir,
-            "dp2",
-            "legacy",
-            "camera.fits",
-        ),
-        find_dataset_or_raise(butler, "camera", instrument="LSSTCam"),
-    )
+    if visit_images:
+        extract_visit_image(
+            butler,
+            os.path.join(
+                testdata_dir,
+                "dp2",
+                "legacy",
+                "visit_image.fits",
+            ),
+            find_dataset_or_raise(butler, "visit_image", **DP2_VISIT_DETECTOR_DATA_ID),
+            shuffle=True,
+            wcs_dataset_ref=find_dataset_or_raise(
+                butler, "visit_summary", **DP2_VISIT_DETECTOR_DATA_ID, collections=wcs_collection
+            ),
+        )
+        extract_visit_image(
+            butler,
+            os.path.join(
+                testdata_dir,
+                "dp2",
+                "legacy",
+                "preliminary_visit_image.fits",
+            ),
+            find_dataset_or_raise(butler, "preliminary_visit_image", **DP2_VISIT_DETECTOR_DATA_ID),
+            shuffle=True,
+        )
+    if coadds:
+        extract_cell_coadd(
+            butler,
+            os.path.join(
+                testdata_dir,
+                "dp2",
+                "legacy",
+                "deep_coadd_cell_predetection.fits",
+            ),
+            find_dataset_or_raise(butler, "deep_coadd_cell_predetection", **DP2_COADD_DATA_ID),
+            shuffle=True,
+        )
+    if camera:
+        extract_camera(
+            butler,
+            os.path.join(
+                testdata_dir,
+                "dp2",
+                "legacy",
+                "camera.fits",
+            ),
+            find_dataset_or_raise(butler, "camera", instrument="LSSTCam"),
+        )
 
 
 if __name__ == "__main__":
