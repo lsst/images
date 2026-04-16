ff06b3d8eae32762eb302dbae90cda53d6883c18
Tim Jenness <tjenness@lsst.org>
2026-04-16 10:14:09 -0700

    Initial copy of ExposureSummaryStats from afw
    Now named ObservationSummaryStats

---
+++ python/lsst/images/_observation_summary_stats.py
@@ -0,0 +1,162 @@
+# This file is part of afw.
+#
+# Developed for the LSST Data Management System.
+# This product includes software developed by the LSST Project
+# (https://www.lsst.org).
+# See the COPYRIGHT file at the top-level directory of this distribution
+# for details of code ownership.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+from __future__ import annotations
+
+__all__ = ("ObservationSummaryStats",)
+
+import dataclasses
+
+
+def _default_corners():
+    return [float("nan")] * 4
+
+
+@dataclasses.dataclass
+class ObservationSummaryStats:
+    version: int = 0
+
+    psfSigma: float = float("nan")
+    """PSF determinant radius (pixels)."""
+
+    psfArea: float = float("nan")
+    """PSF effective area (pixels**2)."""
+
+    psfIxx: float = float("nan")
+    """PSF shape Ixx (pixels**2)."""
+
+    psfIyy: float = float("nan")
+    """PSF shape Iyy (pixels**2)."""
+
+    psfIxy: float = float("nan")
+    """PSF shape Ixy (pixels**2)."""
+
+    ra: float = float("nan")
+    """Bounding box center Right Ascension (degrees)."""
+
+    dec: float = float("nan")
+    """Bounding box center Declination (degrees)."""
+
+    pixelScale: float = float("nan")
+    """Measured detector pixel scale (arcsec/pixel)."""
+
+    zenithDistance: float = float("nan")
+    """Bounding box center zenith distance (degrees)."""
+
+    expTime: float = float("nan")
+    """Exposure time of the exposure (seconds)."""
+
+    zeroPoint: float = float("nan")
+    """Mean zeropoint in detector (mag)."""
+
+    skyBg: float = float("nan")
+    """Average sky background (ADU)."""
+
+    skyNoise: float = float("nan")
+    """Average sky noise (ADU)."""
+
+    meanVar: float = float("nan")
+    """Mean variance of the weight plane (ADU**2)."""
+
+    raCorners: list[float] = dataclasses.field(default_factory=_default_corners)
+    """Right Ascension of bounding box corners (degrees)."""
+
+    decCorners: list[float] = dataclasses.field(default_factory=_default_corners)
+    """Declination of bounding box corners (degrees)."""
+
+    astromOffsetMean: float = float("nan")
+    """Astrometry match offset mean."""
+
+    astromOffsetStd: float = float("nan")
+    """Astrometry match offset stddev."""
+
+    nPsfStar: int = 0
+    """Number of stars used for psf model."""
+
+    psfStarDeltaE1Median: float = float("nan")
+    """Psf stars median E1 residual (starE1 - psfE1)."""
+
+    psfStarDeltaE2Median: float = float("nan")
+    """Psf stars median E2 residual (starE2 - psfE2)."""
+
+    psfStarDeltaE1Scatter: float = float("nan")
+    """Psf stars MAD E1 scatter (starE1 - psfE1)."""
+
+    psfStarDeltaE2Scatter: float = float("nan")
+    """Psf stars MAD E2 scatter (starE2 - psfE2)."""
+
+    psfStarDeltaSizeMedian: float = float("nan")
+    """Psf stars median size residual (starSize - psfSize)."""
+
+    psfStarDeltaSizeScatter: float = float("nan")
+    """Psf stars MAD size scatter (starSize - psfSize)."""
+
+    psfStarScaledDeltaSizeScatter: float = float("nan")
+    """Psf stars MAD size scatter scaled by psfSize**2."""
+
+    psfTraceRadiusDelta: float = float("nan")
+    """Delta (max - min) of the model psf trace radius values evaluated on a
+    grid of unmasked pixels (pixels).
+    """
+
+    psfApFluxDelta: float = float("nan")
+    """Delta (max - min) of the model psf aperture flux (with aperture radius
+    of max(2, 3*psfSigma)) values evaluated on a grid of unmasked pixels.
+    """
+
+    psfApCorrSigmaScaledDelta: float = float("nan")
+    """Delta (max - min) of the psf flux aperture correction factors scaled
+    (divided) by the psfSigma evaluated on a grid of unmasked pixels.
+    """
+
+    maxDistToNearestPsf: float = float("nan")
+    """Maximum distance of an unmasked pixel to its nearest model psf star
+    (pixels).
+    """
+
+    starEMedian: float = float("nan")
+    """Median ellipticity (sqrt(starE1**2.0 + starE2**2.0)) of the stars used
+    in the PSF model.
+    """
+
+    starUnNormalizedEMedian: float = float("nan")
+    """Median un-normalized ellipticity
+    (sqrt((starXX - starYY)**2.0 + (2.0*starXY)**2.0))
+    of the stars used in the PSF model.
+    """
+
+    effTime: float = float("nan")
+    """Effective exposure time calculated from psfSigma, skyBg, and
+    zeroPoint (seconds).
+    """
+
+    effTimePsfSigmaScale: float = float("nan")
+    """PSF scaling of the effective exposure time."""
+
+    effTimeSkyBgScale: float = float("nan")
+    """Sky background scaling of the effective exposure time."""
+
+    effTimeZeroPointScale: float = float("nan")
+    """Zeropoint scaling of the effective exposure time."""
+
+    magLim: float = float("nan")
+    """Magnitude limit at fixed SNR (default SNR=5) calculated from psfSigma,
+     skyBg, zeroPoint, and readNoise.
+    """
